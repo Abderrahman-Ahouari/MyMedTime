59520612e4712bac16510567979c4178a8d4a337
Abderrahmane_Ahouari <abdorrahmane054@gmail.com>
2025-02-05 00:52:25 +0100

    fkfkfkf

--- test.php
+++ test.php
@@ -1,7 +1,8 @@
 <?php
 try {
-    $pdo = new PDO("pgsql:host=localhost;dbname=test", "postgres", "742006");
+    $pdo = new PDO("pgsql:host=localhost;dbname=MYMEDTIME", "postgres", "742006");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo"hhhhhhhh";
 
 } catch (PDOException $e) {
     echo "Erreur de connexion : " . $e->getMessage();
